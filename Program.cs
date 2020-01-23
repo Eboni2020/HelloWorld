b84662cd5a13ca3d84244dcfd17160a56547a774
Eboni <ebonipowell@mail.usf.edu>
2020-01-22 19:51:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display Hello World!
+            Console.WriteLine("Press any key to contine...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
